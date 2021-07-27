bfabc2a4e9bf7e7ff9c62a4da9ff182fae9bb229
kmatsui058 <kmatsui058@gmail.com>
2021-07-27 22:17:41 +0900

    promise化

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import fs from 'fs'
+import fs from 'fs/promises'
 import readline from 'readline'
 import { google } from 'googleapis';
 import { OAuth2Client } from 'google-auth-library/build/src/auth/oauth2client';
@@ -10,10 +10,12 @@ export const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 // time.
 export const TOKEN_PATH = 'token.json'
 main()
-export default function main(): void {
+export default async function main() {
   // Load client secrets from a local file.
-  authorize(credential, listMajors);
+  const auth = await authorize(credential);
+  listMajors(auth)
 }
+
 
 
 
@@ -24,16 +26,19 @@ export default function main(): void {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-export function authorize(credentials: typeof credential, callback: (auth: OAuth2Client) => void): void {
+export async function authorize(credentials: typeof credential): Promise<OAuth2Client> {
   const { client_secret, client_id, redirect_uris } = credentials.installed
   const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
 
   // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback)
-    oAuth2Client.setCredentials(JSON.parse(token as unknown as string))
-    callback(oAuth2Client)
-  })
+  await fs.readFile(TOKEN_PATH)
+    .then(token => {
+      oAuth2Client.setCredentials(JSON.parse(token as unknown as string))
+    })
+    .catch(async err => {
+      await getNewToken(oAuth2Client)
+    })
+  return oAuth2Client
 }
 
 /**
@@ -42,7 +47,7 @@ export function authorize(credentials: typeof credential, callback: (auth: OAuth
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-export function getNewToken(oAuth2Client: OAuth2Client, callback: (auth: OAuth2Client) => void) {
+export async function getNewToken(oAuth2Client: OAuth2Client) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES
@@ -58,18 +63,18 @@ export function getNewToken(oAuth2Client: OAuth2Client, callback: (auth: OAuth2C
       if (err || !token) return console.error('Error while trying to retrieve access token', err)
       oAuth2Client.setCredentials(token)
       // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err)
-        console.log('Token stored to', TOKEN_PATH)
-      })
-      callback(oAuth2Client)
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token))
+        .catch((err) => {
+          if (err) return console.error(err)
+          console.log('Token stored to', TOKEN_PATH)
+        })
     })
   })
 }
 
+
+
 /**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
 export function listMajors(auth: OAuth2Client) {
@@ -77,15 +82,27 @@ export function listMajors(auth: OAuth2Client) {
   sheets.spreadsheets.values.get(
     {
       spreadsheetId: '1axY7ktG7bJC-MaQD4BsnZ6EWRRb6thkHoeQSlsZQx5k',
-      range: 'text-eventStory-change.xml!A3:E8'
+      range: 'text-eventStory-change.xml!A:E'
     },
     (err, res) => {
       if (err || !res) return console.log('The API returned an error: ' + err)
       const rows = res.data.values
       if (rows && rows.length) {
-        // Print columns A and E, which correspond to indices 0 and 4.
-        rows.map((row) => {
-          console.log(`${row[0]}, ${row[4]}`)
+        const titleIndex = rows.findIndex(row => row.find(col => col === 'zType'))
+        const zTypeIndex = rows[titleIndex].findIndex(row => row.includes('zType'))
+        const machineTextIndex = rows[titleIndex].findIndex(row => row.includes('機械翻訳'))
+        const japaneseTextIndex = rows[titleIndex].findIndex(row => row.includes('Japanese'))
+        rows.forEach((row, index) => {
+          if (index > titleIndex) {
+            const zType = row[zTypeIndex]
+            const machineText = row[machineTextIndex]
+            const japaneseText = row[japaneseTextIndex]
+            if (index < 10) {
+              console.log({ zType })
+              console.log({ machineText })
+              console.log({ japaneseText })
+            }
+          }
         })
       } else {
         console.log('No data found.')
